ef5b605ba6cda8fd1ff087af4c3980ba9f251d56
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-27 11:07:26 +0300

    Create model Book in library/models.py and make migrations

--- library/models.py
+++ library/models.py
@@ -1,3 +1,19 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+
+    class CoverChoices(models.TextChoices):
+        HARD = "hard", "Hard"
+        SOFT = "soft", "Soft"
+
+    title = models.CharField(max_length=63, unique=True)
+    author = models.CharField(max_length=63, blank=True, null=True)
+    cover = models.CharField(max_length=63, choices=CoverChoices.choices, default=CoverChoices.SOFT)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=0.00,
+        validators=[MinValueValidator(0.00)],
+    )
