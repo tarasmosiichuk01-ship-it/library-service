a06ba38a73d2d6289264917b734938013dbe772f
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-27 11:43:30 +0300

    Create User model in user/models.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#AUTH_USER_MODEL = "user.User"
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
