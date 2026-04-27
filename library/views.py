496702f4c8365974112eb4012541dac5ed599e42
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-27 17:10:06 +0300

    Create BorrowingView, BorrowingDetailView and BorrowingReturnView

--- library/views.py
+++ library/views.py
@@ -1,9 +1,44 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from library.models import Book
-from library.serializers import BookSerializer
+from library.models import Book, Borrowing
+from library.serializers import BookSerializer, BorrowingSerializer, BookDetailSerializer, BorrowingReturnSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BorrowingView(generics.ListCreateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        user_id = self.request.query_params.get("user_id", None)
+        is_active = self.request.query_params.get("is_active", None)
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+
+        if is_active:
+            if is_active == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        return queryset
+
+
+class BorrowingDetailView(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BookDetailSerializer
+
+
+class BorrowingReturnView(generics.UpdateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReturnSerializer
