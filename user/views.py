5eca13b3c93dbdd5f07192165e481cfaf54ebf6c
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-27 14:06:14 +0300

    Create RegisterUserView & LoginUserView in user/views.py

--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class RegisterUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = ()
+
+
+class LoginUserView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
