4ab16f30818b99c92e96d6840b46bff3e8da7246
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-27 17:10:43 +0300

    Set up urls for borrowings in library/urls.py

--- library/urls.py
+++ library/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from library.views import BookViewSet
+from library.views import BookViewSet, BorrowingView, BorrowingDetailView, BorrowingReturnView
 
 app_name = "library"
 
 router = routers.DefaultRouter()
 router.register("books", BookViewSet)
+#router.register("borrowings", BorrowingViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("borrowings/", BorrowingView.as_view(), name="borrowings"),
+    path("borrowings/<int:pk>/", BorrowingDetailView.as_view(), name="borrowings-detail"),
+    path("borrowings/<int:pk>/return/", BorrowingReturnView.as_view(), name="borrowing-return"),
 ]
 
