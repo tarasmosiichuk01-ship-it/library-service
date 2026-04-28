1f75a4dec9351db4e1780f3d1fa17d2320bcfcbd
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-28 17:52:24 +0300

    Define queryset in BorrowingViewSet in library/views.py

--- library/views.py
+++ library/views.py
@@ -14,6 +14,7 @@ class BookViewSet(viewsets.ModelViewSet):
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
     permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
