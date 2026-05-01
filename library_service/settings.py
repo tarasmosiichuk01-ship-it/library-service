55c51e264312f7e6be50c16f532c07d17dbe2eca
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-05-01 10:33:23 +0300

    Move SECRET_KEY from settings.py to .env

--- library_service/settings.py
+++ library_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from decouple import config
 from datetime import timedelta
 from pathlib import Path
 from celery.schedules import crontab
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@i-9q6*d6n$0ywa=cs*zeug8ojfla(4mln82zzg7gm1y)qtqit"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
